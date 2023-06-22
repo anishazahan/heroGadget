65ede4ffe0710d9efc1d93e79744136d1acfb7be
anishazahan <anishazahan13@gmail.com>
2023-06-22 18:06:07 +0600

    routing system add

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import About from './components/About'
+import Home from './components/Home'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+
+
+const router = createBrowserRouter9([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <ErrorPage />,
+        loader: productsAndCartData,
+        children: [
+          { path: '/', element: <Home /> },
+          { path: '/shop', element: <Shop /> },
+          { path: '/cart', element: <Cart /> },
+          { path: '/about', element: <About /> },
+        ],
+      },
+
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <>
+      <RouterProvider router={router} />
+      <Toaster />
+    </>
+  )
+  
