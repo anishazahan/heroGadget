da478165fe5f1092a91bfa291be898e943c7e217
anishazahan <anishazahan13@gmail.com>
2023-06-27 10:10:57 +0600

    compete cart count section

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,31 +1,47 @@
-import React from 'react'
-import { useLoaderData } from 'react-router-dom'
+import React, { useContext, useState } from 'react'
+import { CartContext, ProductContext } from '../App'
 import ProductCard from './Cards/ProductCard'
-import { addToDb } from '../utils/fakeDb'
-
+import { addToDb } from '../utils/fakeDB'
+import toast from 'react-hot-toast'
 
 const Shop = () => {
+  const products = useContext(ProductContext || [])
+  const [cart, setCart] = useContext(CartContext || [])
 
-    const productData = useLoaderData()
-    
-    const handleAddToCart = id=>{
-        console.log(id);
-        addToDb(id)
-        
-    }
-    return (
-        <div className='my-container'>
-          <div className='product-container'>
-            {productData.map(product => (
-              <ProductCard
-                key={product.id}
-                product={product}
-                handleAddToCart={handleAddToCart}
-              />
-            ))}
-          </div>
-        </div>
+  const handleAddToCart = product => {
+    let newCart = []
+    const exists = cart.find(
+      existingProduct => existingProduct.id === product.id
+    )
+    if (!exists) {
+      product.quantity = 1
+      newCart = [...cart, product]
+    } else {
+      const rest = cart.filter(
+        existingProduct => existingProduct.id !== product.id
       )
+      exists.quantity = exists.quantity + 1
+      newCart = [...rest, exists]
+    }
+
+    setCart(newCart)
+    addToDb(product.id)
+    toast.success('Product Added! 🛒', { autoClose: 500 })
+  }
+
+  return (
+    <div className='my-container'>
+      <div className='product-container'>
+        {products.map(product => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Shop
